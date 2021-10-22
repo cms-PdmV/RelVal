1c3b9e173e3414453bc7f8778f3a6d81138408c2
Justinas Rumševičius <justinasr@users.noreply.github.com>
2021-10-22 17:00:43 +0200

    Check if output datasets exist in workflow (#61)
    * Check if output datasets exist in workflow
    
    * Spaces

--- vue_frontend/src/components/RelVals.vue
+++ vue_frontend/src/components/RelVals.vue
@@ -62,11 +62,11 @@
                 <a v-if="!isDev" target="_blank" title="Open workflow in ReqMgr2" :href="'https://cmsweb.cern.ch/reqmgr2/fetch?rid=' + workflow.name">{{workflow.name}}</a>&nbsp;
                 <a v-if="isDev" target="_blank" title="Open workflow in ReqMgr2" :href="'https://cmsweb-testbed.cern.ch/reqmgr2/fetch?rid=' + workflow.name">{{workflow.name}}</a>&nbsp;
                 <template v-if="!isDev">
-                  <small>open in:</small> <a target="_blank" title="Open workflow in Stats2" :href="'https://cms-pdmv.cern.ch/stats?workflow_name=' + workflow.name">Stats2</a>&nbsp;
+                  <small> open in:</small> <a target="_blank" title="Open workflow in Stats2" :href="'https://cms-pdmv.cern.ch/stats?workflow_name=' + workflow.name">Stats2</a>&nbsp;
                 </template>
-                <span v-if="workflow.status_history && workflow.status_history.length > 0">
-                  <small>status:</small> {{workflow.status_history[workflow.status_history.length - 1].status}}
-                </span>
+                <template v-if="workflow.status_history && workflow.status_history.length > 0">
+                  <small> status:</small> {{workflow.status_history[workflow.status_history.length - 1].status}}
+                </template>
                 <ul v-if="index == item.workflows.length - 1" class="zebra-datasets">
                   <li v-for="dataset in workflow.output_datasets" :key="dataset.name">
                     <div>
@@ -348,11 +348,13 @@ export default {
         component.dataItems.forEach(item => {
           if (item.workflows && item.workflows.length) {
             const lastWorkflow = item.workflows[item.workflows.length - 1];
-            lastWorkflow.output_datasets.forEach(ds => {
-              ds.datatier = ds.name.split('/').pop();
-              ds.completed = (lastWorkflow.total_events > 0 ? (ds.events / lastWorkflow.total_events * 100) : 0).toFixed(2);
-              ds.niceEvents = ds.events.toLocaleString('en-US');
-            })
+            if (lastWorkflow.output_datasets) {
+              lastWorkflow.output_datasets.forEach(ds => {
+                ds.datatier = ds.name.split('/').pop();
+                ds.completed = (lastWorkflow.total_events > 0 ? (ds.events / lastWorkflow.total_events * 100) : 0).toFixed(2);
+                ds.niceEvents = ds.events.toLocaleString('en-US');
+              });
+            }
           }
         });
         component.totalItems = response.data.response.total_rows;
