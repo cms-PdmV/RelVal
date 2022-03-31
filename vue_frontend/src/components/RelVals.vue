7e291f2b601d4860daa4a0e5653b877e09980fee
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2022-03-31 14:33:49 +0200

    Correctly unset timeout when doing next status

--- vue_frontend/src/components/RelVals.vue
+++ vue_frontend/src/components/RelVals.vue
@@ -434,7 +434,7 @@ export default {
       const submit = function () {
         component.clearDialog();
         component.loading = true;
-        axios.post('api/relvals/next_status', {timeout: 0, data: relvals.slice()}).then(() => {
+        axios.post('api/relvals/next_status', relvals.slice(), {timeout: 0}).then(() => {
           component.fetchObjects();
           component.selectedItems = [];
         }).catch(error => {
